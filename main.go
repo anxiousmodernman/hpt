65fd29df6f2a4cba0653f0e2fa888c09f67fad42
Coleman McFarland <coleman.mcfarland@gmail.com>
2017-10-17 15:08:11 -0700

    remove dead code

--- main.go
+++ main.go
@@ -90,31 +90,3 @@ func run(paths ...string) error {
 func plan(paths ...string) error {
 	return nil
 }
-
-// flag.Parse()
-// accessKey := os.Getenv("DO_ACCESS_KEY")
-// secKey := os.Getenv("DO_SECRET_ACCESS_KEY")
-// ssl := true
-
-// // fetch your own config
-// conf, err := NewConfig(*confPath)
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// _ = conf
-
-// // Initiate a client using DigitalOcean Spaces.
-// client, err := minio.New("nyc3.digitaloceanspaces.com", accessKey, secKey, ssl)
-// if err != nil {
-// 	log.Fatal(err)
-// }
-
-// opts := minio.GetObjectOptions{}
-// obj, err := client.GetObject("coleman", "ssh-keys/hackbox", opts)
-// if err != nil {
-// 	log.Fatal(err)
-// }
-// data, err := ioutil.ReadAll(obj)
-// if err := ioutil.WriteFile("hackbox", data, 0600); err != nil {
-// 	log.Fatal(err)
-// }
