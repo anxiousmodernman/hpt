bbfefa6d729e254ee4e42b2d5e7029f03e9d6720
Coleman McFarland <coleman.mcfarland@gmail.com>
2017-11-03 17:16:32 -0700

    add a hook for uninstalling packages

--- package.go
+++ package.go
@@ -23,7 +23,7 @@ func ApplyInstallPackages(conf Config, packages []string) *ApplyState {
 	// this packages = [ "foo", "baz", "zaz" ] syntax into the higher level
 	// config's Packages slice. Here we represent many states with one, and
 	// return early on any error. Sad!
-	state := &ApplyState{}
+	state := NewApplyState("package")
 	for _, pkg := range packages {
 		p := Package{pkg, Installed}
 		state = ApplyPackage(conf, p)
@@ -45,6 +45,8 @@ func ApplyPackage(conf Config, p Package) *ApplyState {
 	switch p.State {
 	case Installed:
 		return state.Error(redhatInstall(p.Name, output))
+	case Absent:
+		return state.Error(redhatUninstall(p.Name, output))
 	}
 
 	return state.Error(fmt.Errorf("unknown package state: %v", p.State))
@@ -55,9 +57,20 @@ func redhatInstall(name string, output *bytes.Buffer) error {
 
 	out, err := cmd.Output()
 	if err != nil {
+		// We can rely on state.Error to capture the stderr from this error.
 		return err
 	}
 	// TODO this was changed
+	output.Write(out)
+	return nil
+}
+func redhatUninstall(name string, output *bytes.Buffer) error {
+	cmd := exec.Command("yum", "-y", "erase", name)
+
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
 	output.Write(out)
 	return nil
 }
