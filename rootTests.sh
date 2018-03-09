da2771d1ae48b86111ae82d3056c8adb76ba308a
Coleman McFarland <coleman.mcfarland@gmail.com>
2018-03-08 20:06:28 -0800

    Move root tests from ion to regular bash
    Unfortunately, ion shell hasn't conquered the world.

--- rootTests.sh
+++ rootTests.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env ion
+#!/bin/bash
 
 # Unfortunately, some of our tests need to run as root. We hide them behind 
 # a t.Skip call in our Go code, and we must call them by name here.
 
 # Root does not have a GOPATH, so we provide ours. Here ya go, root.
-export GOPATH = "/home/coleman/go"
+export GOPATH="/home/coleman/go"
 
 go test -run CreateFileWithPermissions
 
